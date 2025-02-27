aeaf231c5763a4f3e9bd4bdf074fdd2444b3ffd2
Kristina Cas <m.kristinacas@gmail.com>
2025-02-28 01:27:41 +0800

    Added SPACE and BACK buttons + fixed button sizes

--- mainkey.py
+++ mainkey.py
@@ -9,14 +9,15 @@ from time import sleep
 
 # for launching camera
 cap = cv2.VideoCapture(0)
-cap.set(3,1280)
+cap.set(3,1950)
 cap.set(4,720)
 
 # hand detection + keyboard letters + text var initialization
 detector = HandDetector(detectionCon=0.8 , maxHands=2)
 keys = [["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
         ["A", "S", "D", "F", "G", "H", "J", "K", "L", ":"],
-        ["Z", "X", "C", "V", "B", "N", "M", ",", ".", "/"]]
+        ["Z", "X", "C", "V", "B", "N", "M", ",", ".", "/"],
+        ["SPACE", "BACK"]]
 finalText = ""
 
 
@@ -39,7 +40,13 @@ class Button():
 buttonList = []
 for i in range(len(keys)):
     for j, key in enumerate(keys[i]):
-        buttonList.append(Button([100 * j + 50, 100 * i + 50], key))
+        #buttonList.append(Button([100 * j + 50, 100 * i + 50], key))
+        if key == "SPACE":
+            buttonList.append(Button([300, 100 * i + 50], key, size=(265, 75)))
+        elif key == "BACK":
+            buttonList.append(Button([600 * j + 50, 100 * i + 50], key, size=(210,75)))
+        else: 
+            buttonList.append(Button([100 * j + 50, 100 * i + 50], key))
 
 # for function of the whole keyboard
 while True:
@@ -75,12 +82,19 @@ while True:
                  if l<30:
                     cv2.rectangle(img, button.pos, (x + w, y + h), (0, 255, 0), cv2.FILLED)
                     cv2.putText(img, button.text, (x + 25, y + 60), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
-                    finalText += button.text
+
+                    if button.text == "SPACE":
+                        finalText = finalText + " "
+                    elif button.text == "BACK":
+                        finalText = finalText[:-1]
+                    else: 
+                        finalText += button.text
+
                     sleep(0.90)
 
     #for textbox           
-    cv2.rectangle(img, (50, 350), (700, 450), (175, 0, 175), cv2.FILLED)
-    cv2.putText(img, finalText, (75, 420), cv2.FONT_HERSHEY_PLAIN, 5, (255, 255, 255), 5)
+    cv2.rectangle(img, (70, 470), (1000, 680), (175, 0, 175), cv2.FILLED)
+    cv2.putText(img, finalText, (70, 550), cv2.FONT_HERSHEY_PLAIN, 5, (255, 255, 255), 5)
     
     cv2.imshow("Image", img)
 
