1a1c4ef67216dacd94790b3306e2ade73d782b0a
Kristina Cas <m.kristinacas@gmail.com>
2025-02-27 16:54:20 +0800

    Button Class Added (No concrete button deployed)

--- mainkey.py
+++ mainkey.py
@@ -10,6 +10,19 @@ cap.set(4,720)
 
 detector = HandDetector(detectionCon=int(0.8 * 100), maxHands=2)
 
+class Button():
+    def __init__(self, pos, text, size=[85, 85]):
+        self.pos = pos
+        self.size = size
+        self.text = text
+    
+    def draw(self, img):
+        x, y = self.pos
+        w, h = self.size
+        cv2.rectangle(img, self.pos, (x + w, y + h), (255, 0, 255), cv2.FILLED)
+        cv2.putText(img, self.text, (x + 25, y + 60), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
+        return img
+
 while True:
     success, img = cap.read()
     img = detector.findHands(img)
